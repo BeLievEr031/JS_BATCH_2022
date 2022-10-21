362b7bd240196ea84a000a6a5666584f7228a91f
BeLievEr031 <sandyrajak031@gmail.com>
2022-10-21 22:07:20 +0530

    added upto condition . variable

---
+++ 22Oct2022/condition.js
@@ -0,0 +1,53 @@
+
+// // if("barish ho rahii he"){
+// // console.log("mai umbrella lekar niklunga");
+// // }else{
+// //     console.log("mai umbrella lekar nahi niklunga");
+// // }
+
+// let num1 = 10;
+// let num2 = 20;
+
+// if (num1 > num2) {
+
+//   console.log("num1 is greater..");
+
+// } else {
+
+//   console.log("num2 is greater..");
+
+// }
+
+// let num1 = 720;
+// let num2 = 60;
+// let num3 = 40;
+
+// 1800
+// 4000 goa
+// < 2000 palghar
+
+// if (3500 == 4000) {
+//   console.log("goa wala friend");
+// } else if (3500 < 2000) {
+//   console.log("palghar wala friend");
+// } else {
+//   console.log("boisar wala friend");
+// }
+
+// var let
+
+{
+    // XYZ
+    var country = "india";
+    console.log("line 40", country);
+  }
+  
+  
+  
+  {
+    // @abc
+    console.log("line 49", country);
+    var country = 50;
+    console.log("line 52", country);
+  }
+  
---
+++ 22Oct2022/varable.js
@@ -0,0 +1,35 @@
+// javascript 10 days Brenden Eich
+// var --> keyword
+// var
+// var let const
+
+// var country = "India";
+// // let state = "goa";
+// // const pi = 3.14;
+
+// console.log(country)
+// console.log(state);
+// console.log(pi);
+
+// var country ="india";
+// console.log(country);
+
+// country = "USA";
+// console.log(country);
+
+// let fruit = "apple";
+// fruit = 31;
+// console.log(fruit);
+
+// const gender = "M";
+// gender = "F"
+// console.log(gender);
+
+
+
+
+
+
+
+
+
