239d3dce24a68e69aece185c56907f76ee15f83f
BeLievEr031 <sandyrajak031@gmail.com>
2022-10-26 21:25:11 +0530

    done upto fs module 1/2

---
+++ 26Oct2022/TODO.md
@@ -0,0 +1,20 @@
+[+] Function
+[+] loops
+
+@File Module
+[+] fs.writeFileSync()
+[+] fs.readFileSync()
+[+] fs.unlinkSync()
+[+] fs.mkdirSync()
+[+] fs.rmdirSync()
+
+
+[-] fs.appendFileSync()
+[-] fs.existSync()
+[-] fs.lstatSync().isFile()
+[-] fs.readDirSync()
+
+@Path Module
+
+[-] path.extname()
+[-] path.extname()
---
+++ 26Oct2022/fsModule.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+let fs = require("fs");
+
+// mkdirSync()
+
+// fs.mkdirSync("fruits");
+
+// fs.writeFileSync("abc.txt", "i am a text file.", function (err) {
+//   console.log(err);
+// });
+
+// fs.writeFileSync("suri.txt", "my name is srikant suri..",function(err){
+//     console.log(err);
+// });
+
+// let data = fs.readFileSync("abc.txt");
+// console.log(data + "");
+
+// fs.unlinkSync("abc.txt");
+
+// fs.rmdirSync("fruits")
